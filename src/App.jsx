8237b0d584eee7df42ba1eb4e34155fe0d2d63ad
haram8009 <haram8009@gmail.com>
2023-05-10 10:24:29 +0900

    기본 구조 다지기

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Routes, Route } from "react-router";
+import { Home } from "./pages";
+import { Header } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Header() {
+  return <p>헤더</p>;
+}
+export default Header;
