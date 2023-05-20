bb7acd9198ae2185333bd6dc46a87afa67c92d67
hyeesw <hyeeun7904@kookmin.ac.kr>
2023-05-20 23:07:26 +0900

    버튼 눌렀을 때 모달 뜨도록

--- src/components/PointForm.jsx
+++ src/components/PointForm.jsx
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function PointForm() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
+    <>
     <CardGroup className="text-center">
       <Card>
         <Card.Body>
-          <Button variant="primary"><Card.Title>1,000 P</Card.Title></Button>
+          <Button onClick={handleShow}><Card.Title>1,000 P</Card.Title></Button>
         </Card.Body>
         <Card.Footer className="text-muted">Cagong Point</Card.Footer>
       </Card>
       <Card>
         <Card.Body>
-        <Button variant="primary"><Card.Title>5,000 P</Card.Title></Button>
+        <Button onClick={handleShow}><Card.Title>5,000 P</Card.Title></Button>
         </Card.Body>
         <Card.Footer className="text-muted">Cagong Point</Card.Footer>
       </Card>
       <Card>
         <Card.Body>
-        <Button variant="primary"><Card.Title>10,000 P</Card.Title></Button>
+        <Button onClick={handleShow}><Card.Title>10,000 P</Card.Title></Button>
         </Card.Body>
         <Card.Footer className="text-muted">Cagong Point</Card.Footer>
       </Card>
     </CardGroup>
+
+    <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>포인트 결제</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <p>기존 포인트 : 4000</p> 
+          <p>선택된 포인트 : 10000</p>
+          <p>충전 후 포인트 : 14000</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            결제하기
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            닫기
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+    
   );
 }
 
