162d5a6c175614250bf524fc95ff7adfaa46ef25
jm3789 <jm37890@gmail.com>
2023-05-25 04:09:55 +0900

    #6 유저 닉네임 반영, 프로필 창에서 로그인, 로그인된 상태일 때 프로필에 테두리

--- src/components/CustomToggle.jsx
+++ src/components/CustomToggle.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
 
-function CustomToggle({ children, onClick, userInfo }) {
-  const navigate = useNavigate();
+function CustomToggle({ children, onClick }) {
 
   const handleClick = (e) => {
-    if (!userInfo) {
-      alert('로그인 후 이용해주세요.');
-      navigate('/signin');
-    } else {
-      onClick(e);
-    }
+    onClick(e);
   };
 
   return (
@@ -25,7 +18,6 @@ function CustomToggle({ children, onClick, userInfo }) {
 CustomToggle.propTypes = {
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func.isRequired,
-  userInfo: PropTypes.bool.isRequired,
 };
 
 export default CustomToggle;
--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -13,9 +13,9 @@ function MyProfile({ userInfo }) {
   const [showOffcanvas, setShowOffcanvas] = useState(false);
 
   const handleShowOffcanvas = () => {
-    if (userInfo) { 
+    if (userInfo) {
       setShowOffcanvas(true);
-    } else { 
+    } else {
       alert('로그인 후 이용 가능합니다.'); // 로그인되지 않은 상태에는 canvas 창을 열 수 없고 alert
     }
   };
@@ -24,22 +24,27 @@ function MyProfile({ userInfo }) {
     setShowOffcanvas(false);
   };
 
+  const handleSigninClick = (e) => {
+    e.preventDefault();
+    navigate('/signin');
+  };
+
   const handleSignoutClick = (e) => {
     e.preventDefault();
     alert('로그아웃 하시겠습니까?');
     removeUser();
     navigate('/');
   };
-
+ 
   // 프로필 사진 자리에 들어갈 것
   let SmallProfilePhoto;
   let BigProfilePhoto;
   if (userInfo) { // 로그인된 상태
-    SmallProfilePhoto = <div className="circle" style={{ width: "50px", height: "50px", borderRadius: "50%", backgroundColor: "grey" }}></div>
-    BigProfilePhoto = <div className="circle" style={{ width: "100px", height: "100px", borderRadius: "50%", backgroundColor: "grey", textAlign: "center" }}></div>
+    SmallProfilePhoto = <div className="circle" style={{ width: "50px", height: "50px", borderRadius: "50%", backgroundColor: "lightgrey", border: "3px solid green"}}></div>
+    BigProfilePhoto = <div className="circle" style={{ width: "100px", height: "100px", borderRadius: "50%", backgroundColor: "lightgrey", textAlign: "center", border: "4px solid green" }}></div>
   } else {  // 로그인되지 않은 상태
-    SmallProfilePhoto = <p>프로필</p>;
-    BigProfilePhoto = <p>프로필</p>;
+    SmallProfilePhoto = <div className="circle" style={{ width: "50px", height: "50px", borderRadius: "50%", backgroundColor: "lightgrey" }}></div>
+    BigProfilePhoto = <div className="circle" style={{ width: "100px", height: "100px", borderRadius: "50%", backgroundColor: "lightgrey", textAlign: "center" }}></div>
   }
 
   return (
@@ -52,26 +57,45 @@ function MyProfile({ userInfo }) {
         >
           {SmallProfilePhoto}
         </Dropdown.Toggle>
+
         <Dropdown.Menu style={{ minWidth: '400px', padding: '20px'}}>
-          <Dropdown.Item onClick={handleShowOffcanvas} style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <img src="/images/bell.png" alt="알림버튼" style={{ width: "30px", height: "30px" }}/>
-          </Dropdown.Item>
-          <div className="d-flex align-items-center justify-content-center mb-3">
-            {BigProfilePhoto}
-          </div>
-          <Dropdown.Item href="#/action-3" disabled style={{ fontSize: '32px', color: 'black', textAlign: 'center' }}>
-            (name)
-          </Dropdown.Item>
-          <Dropdown.Item href="#/action-4" disabled style={{ textAlign: 'center' }}>손님</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item href="#/action-5" onClick={(e) => handleSignoutClick(e)} style={{ fontSize: '20px', color: 'grey', textAlign: 'center' }}>
-            로그아웃
-          </Dropdown.Item>
+          {userInfo 
+            ?  // 로그인 O
+            <>
+              <Dropdown.Item onClick={handleShowOffcanvas} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <img src="/images/bell.png" alt="알림버튼" style={{ width: "30px", height: "30px" }}/>
+              </Dropdown.Item>
+              <div className="d-flex align-items-center justify-content-center mb-3">
+                {BigProfilePhoto}
+              </div>
+              <Dropdown.Item href="#/action-3" disabled style={{ fontSize: '32px', color: 'black', textAlign: 'center' }}>
+                {userInfo['username']}  {/* 로그인 후, localStorage에 저장되어있던 userInfo에서, 'username'의 value값 */}
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-4" disabled style={{ textAlign: 'center' }}>손님</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item href="#/action-5" onClick={(e) => handleSignoutClick(e)} style={{ fontSize: '20px', color: 'grey', textAlign: 'center' }}>
+                로그아웃
+              </Dropdown.Item>
+            </>
+            :  // 로그인 X
+            <>
+              <br />
+              <Dropdown.Item href="#/action-3" disabled style={{ fontSize: '18px', color: 'grey', textAlign: 'center' }}>
+                로그인이 필요한 서비스입니다.
+              </Dropdown.Item>
+              <br />
+              <Dropdown.Divider />
+              <Dropdown.Item href="#/action-5" onClick={(e) => handleSigninClick(e)} style={{ fontSize: '20px', color: 'black', textAlign: 'center' }}>
+                로그인
+              </Dropdown.Item>
+            </>
+          }
         </Dropdown.Menu>
       </Dropdown>
+
       <Offcanvas show={showOffcanvas} onHide={handleCloseOffcanvas} placement="end">
       <Offcanvas.Header closeButton>
-        <Offcanvas.Title style={{ fontSize: '32px' }}>
+        <Offcanvas.Title style={{ fontSize: '26px' }}>
           알림
         </Offcanvas.Title>
       </Offcanvas.Header>
