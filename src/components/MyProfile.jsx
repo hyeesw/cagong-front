00e610eb82eb85554aeafb99a436f83c1a495c60
haram8009 <haram8009@gmail.com>
2023-05-25 22:38:41 +0900

    chore

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -18,31 +18,49 @@ function MyProfile() {
     <>
       <Dropdown>
         <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-          <img src="/images/profile.jpg" alt="프로필이미지" style={{ width: "50px", height: "50px" }}/>
+          <img
+            src="/images/profile.jpg"
+            alt="프로필이미지"
+            style={{ width: '50px', height: '50px' }}
+          />
         </Dropdown.Toggle>
         <Dropdown.Menu style={{ minWidth: '400px', padding: '20px' }}>
-          <Dropdown.Item onClick={handleShow} style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <img src="/images/bell.png" alt="알림버튼" style={{ width: "30px", height: "30px" }}/>
+          <Dropdown.Item
+            onClick={handleShow}
+            style={{ display: 'flex', justifyContent: 'flex-end' }}
+          >
+            <img src="/images/bell.png" alt="알림버튼" style={{ width: '30px', height: '30px' }} />
           </Dropdown.Item>
           <div className="d-flex align-items-center justify-content-center mb-3">
-            <img src="/images/profile.jpg" alt="프로필이미지" style={{ width: "100px", height: "100px", textAlign: 'center' }}/>
+            <img
+              src="/images/profile.jpg"
+              alt="프로필이미지"
+              style={{ width: '100px', height: '100px', textAlign: 'center' }}
+            />
           </div>
-          <Dropdown.Item href="#/action-3" disabled style={{ fontSize: '32px', color: 'black', textAlign: 'center' }}>
+          <Dropdown.Item
+            href="#/action-3"
+            disabled
+            style={{ fontSize: '32px', color: 'black', textAlign: 'center' }}
+          >
             (name)
           </Dropdown.Item>
-          <Dropdown.Item href="#/action-4" disabled style={{ textAlign: 'center' }}>손님</Dropdown.Item>
+          <Dropdown.Item href="#/action-4" disabled style={{ textAlign: 'center' }}>
+            손님
+          </Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Item href="#/action-5" style={{ fontSize: '20px', color: 'grey', textAlign: 'center' }}>
+          <Dropdown.Item
+            href="#/action-5"
+            style={{ fontSize: '20px', color: 'grey', textAlign: 'center' }}
+          >
             로그아웃
           </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
       <Offcanvas show={showOffcanvas} onHide={handleClose} placement="end">
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title style={{ fontSize: '32px' }}>
-          알림
-        </Offcanvas.Title>
-      </Offcanvas.Header>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title style={{ fontSize: '32px' }}>알림</Offcanvas.Title>
+        </Offcanvas.Header>
         <Offcanvas.Body>
           <Push />
           <Push />
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,22 +4,14 @@ import { SignupForm } from '../components';
 
 function Signup() {
   return (
-    <>
-      <Modal
-        show={true}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header>
-          <Modal.Title id="example-custom-modal-styling-title">
-            회원가입페이지
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <SignupForm />
-        </Modal.Body>
-      </Modal>
-    </>
+    <Modal show backdrop="static" keyboard={false}>
+      <Modal.Header>
+        <Modal.Title id="example-custom-modal-styling-title">회원가입페이지</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <SignupForm />
+      </Modal.Body>
+    </Modal>
   );
 }
 export default Signup;
