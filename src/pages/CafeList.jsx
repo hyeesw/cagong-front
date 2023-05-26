e011cba556d3fa54fdb435813d5c7cbd3491e646
hyeesw <hyeeun7904@kookmin.ac.kr>
2023-05-26 23:50:13 +0900

    검색 기능 (백엔드 연동 전)

--- src/pages/CafeList.jsx
+++ src/pages/CafeList.jsx
@@ -1,11 +1,10 @@
  /* eslint-disable */
- import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import React, { useState } from 'react';
 import { Button, Col, Container, Form, Row, Card } from "react-bootstrap";
 
 function CardArray() {
     let [dataArray, setDataArray] = useState(['해물찜', '라면', '스시', '육회']);
-  
     return(
         <Container>
             {
@@ -37,10 +36,60 @@ function CardArray() {
   }
 
 function CafeList() {
+  const [searchValue, setSearchValue] = useState(""); //검색값
+
+  const countries = [
+
+    { name: "Belgium", continent: "Europe" },
+    { name: "India", continent: "Asia" },
+    { name: "Bolivia", continent: "South America" },
+    { name: "Ghana", continent: "Africa" },
+    { name: "Japan", continent: "Asia" },
+    { name: "Canada", continent: "North America" },
+    { name: "New Zealand", continent: "Australasia" },
+    { name: "Italy", continent: "Europe" },
+    { name: "South Africa", continent: "Africa" },
+    { name: "China", continent: "Asia" },
+    { name: "Paraguay", continent: "South America" },
+    { name: "Usa", continent: "North America" },
+    { name: "France", continent: "Europe" },
+    { name: "Botswana", continent: "Africa" },
+    { name: "Spain", continent: "Europe" },
+    { name: "Senegal", continent: "Africa" },
+    { name: "Brazil", continent: "South America" },
+    { name: "Denmark", continent: "Europe" },
+    { name: "Mexico", continent: "South America" },
+    { name: "Australia", continent: "Australasia" },
+    { name: "Tanzania", continent: "Africa" },
+    { name: "Bangladesh", continent: "Asia" },
+    { name: "Portugal", continent: "Europe" },
+    { name: "Pakistan", continent: "Asia" },
+  
+  ];
+
+  //검색창의 값이 바뀔 때마다 실행됨. (검색값 저장)
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log('불려짐')
+    setSearchValue(e.target.value);
+  }
+
+  //[검색] 버튼 눌렀을 때 실행됨. (데이터 전송)
+  const handleClick = (e) => {
+    e.preventDefault();
+    if(searchValue.length > 0) {
+      //일치하는 값 찾는 내용 (아마도 서버로)
+      countries.filter((country) => {
+        console.log(country.name.match(searchValue));
+        console.log('값은 : ', searchValue)    
+      });
+    }
+  }
+
+
 
   return (
     <>
-
     {/* 검색창 */}
     <Container className="mt-5">
       <Row>
@@ -51,8 +100,11 @@ function CafeList() {
               placeholder="Search"
               className="me-2 rounded-pill"
               aria-label="Search"
+              id="inputValue"
+              onChange={handleChange}
+              value={searchValue}
             />
-            <Button className="rounded-pill" variant="outline-primary">
+            <Button type="submit" className="rounded-pill" variant="outline-primary" onClick={handleClick} >
               Search
             </Button>
           </Form>
