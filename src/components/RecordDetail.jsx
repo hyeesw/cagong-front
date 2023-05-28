0b1089df2a92ca029a2242aa3268956d360c6d81
jm3789 <jm37890@gmail.com>
2023-05-29 02:24:18 +0900

    #22 캘린더 오른쪽 상세정보에 record 적용(테스트 전)

--- src/components/RecordDetail.jsx
+++ src/components/RecordDetail.jsx
@@ -8,21 +8,24 @@ import { Card, CardGroup } from 'react-bootstrap';
 // 공부 시작한 시간 ~ 공부 끝난 시간
 // 메모(null일 시 '메모가 없습니다.')
 
-function RecordDetail() {
+const RecordDetail = ({ records }) => {
+  // 도착한 records 확인
+  console.log(records);
+
   return (
     <CardGroup>
       <Card className="shadow mx-3" style={{ width: '400px' }}>
         <Card.Body>
           <Card.Title>
-            <div style={{fontSize:"18px"}}>2023.5.29</div>
-            <div style={{fontSize:"40px", fontWeight:"bold", color:"mediumpurple"}}>04:31:01</div>
+            <div style={{fontSize:"18px"}}>{records.date}</div>
+            <div style={{fontSize:"40px", fontWeight:"bold", color:"mediumpurple"}}>{records.duration}</div>
           </Card.Title>
           <Card.Subtitle>
-            <div style={{fontWeight:"lighter", color:"grey"}}>9:00:00 ~ 13:31:01</div>
+            <div style={{fontWeight:"lighter", color:"grey"}}>{records.start} ~ {records.end}</div>
           </Card.Subtitle>
         </Card.Body>
         <Card.Footer className="text-muted">
-          <p>메모가 없습니다.</p>
+          <p>{records.memo ? records.memo : '메모가 없습니다.'}</p>
         </Card.Footer>
       </Card>
     </CardGroup>
