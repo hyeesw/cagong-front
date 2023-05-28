c96df40d250e42c8f055a3362d004ff71cfe194f
haram8009 <haram8009@gmail.com>
2023-05-29 03:20:59 +0900

    데이터 뿌리깅

--- src/pages/CafeDetail.jsx
+++ src/pages/CafeDetail.jsx
@@ -1,95 +1,112 @@
+/* eslint-disable */
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams  } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
-import { Menu } from '../components';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import axios from 'axios';
 import { API_URL } from '../constants';
-
+import { Menu } from '../components';
+import { getUser } from '../util/localstorage';
 
 function CafeDetail() {
-    // localhost:3000/cafelist/detail/1 이 들어왔을 떄, cafe_id=1로 저장됨.
-    let { cafe_id } = useParams(); 
-    //back에 get으로 request를 보내고, back에서 response 받아서 화면 랜더링.
-    useEffect(() => {
-        axios // axios로 서버에 요청 보내는 부분 시작!
-          .post(
-            `${API_URL}detail/${cafe_id}/`
-          )
-          .then((response) => {
-            console.log(response.data.cafe_detail); //respone 된 cafe_detail
-            console.log(response.data.menu_list); //respone 된 menu_list
-            //여기서 usestate로 menu값 변경하는 코드 짜면 될 것 같어!!!!!!!!!
-          })
-          .catch((err) => {
-            console.log('메뉴 리스트 get err : ', err);
-          });
-    })
-    
-    /*
-    내 생각엔 location이랑 navigate는 안 써도 될 것 같고, 그냥 21번 라인에서 받은 data를 잘 가공하면 될 것 같아!!
-    */
-    
-    
-    
-    
-    // const location = useLocation();
-    // const { cafeObj } = location.state;
-    // const navigate = useNavigate();
+  const [cafeObj, setcafeObj] = useState({});
+  const [menus, setmenus] = useState([]);
+  // localhost:3000/cafelist/detail/1 이 들어왔을 떄, cafeId=1로 저장됨.
+  const { cafeId } = useParams();
+  const navigate = useNavigate();
 
-    
-    // const [showFullText, setShowFullText] = useState(false);
-    
-    // useEffect(() => {
-    //     setMenus(cafeObj.menu);
-    // }, []);
+  const getCafeDetail = async () => {
+    const result = await axios // axios로 서버에 요청 보내는 부분 시작!
+      .post(`${API_URL}cafe/detail/${cafeId}/`)
+      .then((response) => {
+        console.log('cafe_detail', response.data.cafe_detail);
+        console.log('menu_list', response.data.menu_list);
+        setcafeObj(JSON.parse(response.data.cafe_detail));
+        setmenus([...response.data.menu_list]);
+      })
+      .catch((err) => {
+        console.log('카페상세 get err : ', err);
+      });
+    return result;
+  };
 
-    // const handleShowFullText = () => {
-    //     if (!showFullText) {setShowFullText(true)}
-    //     else {setShowFullText(false)}
-    // }
+  const init = async () => {
+    const userInfo = await getUser();
+    if (!userInfo) {
+      // console.log('회원정보가 없거나 토큰무효');
+      alert('로그인이 필요합니다.');
+      navigate('/signin');
+      return;
+    }
+    console.log('init');
+    await getCafeDetail();
+  };
 
-    // return (
-    //     <Container fluid>
-    //         <Row>
-    //             <Col xs={12} md={6} >
-    //                 <div style={{ width: "60%", margin: "0 auto", textAlign: "center" }}>
-    //                     <div style={{textAlign: "center"}}>
-    //                         <div style={{ width:"100%", aspectRatio: "1/1", backgroundColor: "lightgrey", margin:"30px auto"}}></div> {/* 세로방향 | 가로방향 */}
-    //                         <h4>{cafeObj.name}</h4>
-    //                         <p style={{color:"grey"}}>{cafeObj.location}</p>
-    //                     </div>
-    //                     <div style={{width: "100%", color:"grey", textAlign: "left"}}>
-    //                         <p className="overflow-text" 
-    //                             style={{ whitespace: "nowrap", overflow: "hidden", textoverflow: "ellipsis" }}>
-    //                                 {showFullText ? cafeObj.detail : cafeObj.detail.slice(0, 35)} 
-    //                                 {cafeObj.detail.length > 35 && ( // 텍스트 길이가 일정 이상인 경우에만 ...과 더보기 링크를 추가
-    //                                 <span 
-    //                                 className="read-more" 
-    //                                 style={{ color: "mediumpurple", cursor: "pointer" }} 
-    //                                 onClick={handleShowFullText}>{showFullText ? "닫기" : "...더보기"}</span>
-    //                             )}
-    //                         </p>
-    //                         {/* <img src="/images/call.png" alt="알림버튼" style={{ width: "20px", height: "20px" }}/>
-    //                         <p>{cafeObj.phone}</p> */}
-    //                     </div>
-    //                 </div>
-    //             </Col>
+  useEffect(() => {
+    init();
+  }, []);
 
-    //             <Col xs={12} md={6}>
-    //                 <Container fluid style={{marginTop: "30px", marginBottom: "150px"}}>
-    //                     <Row xs={1} sm={3} md={5} lg={7} xl={9} xxl={11} className="g-3" style={{ gap: '1rem' }}>
-    //                         {menus.map((menu) => (
-    //                         <Col key={menu.id} margin="5px">
-    //                             <Menu menuObj={menu} />
-    //                         </Col>
-    //                         ))}
-    //                     </Row>
-    //                 </Container>
-    //             </Col>
-    //         </Row>
-    //     </Container>
-    // )
+  useEffect(() => {
+    console.log(menus, cafeObj);
+  }, [menus, cafeObj]);
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={12} md={6}>
+          <div style={{ width: '60%', margin: '0 auto', textAlign: 'center' }}>
+            <div style={{ textAlign: 'center' }}>
+              <div
+                style={{
+                  width: '100%',
+                  aspectRatio: '1/1',
+                  backgroundColor: 'lightgrey',
+                  margin: '30px auto',
+                }}
+              />
+              {/* 세로방향 | 가로방향 */}
+              <h4>{cafeObj.name}</h4>
+              <p style={{ color: 'grey' }}>{cafeObj.location}</p>
+            </div>
+            <div style={{ width: '100%', color: 'grey', textAlign: 'left' }}>
+              <p
+                className="overflow-text"
+                style={{ whitespace: 'nowrap', overflow: 'hidden', textoverflow: 'ellipsis' }}
+              >
+                {cafeObj.info}
+              </p>
+              <img
+                src="/images/call.png"
+                alt="알림버튼"
+                style={{ width: '20px', height: '20px' }}
+              />
+              <p>{cafeObj.phone}</p>
+            </div>
+          </div>
+        </Col>
+
+        <Col xs={12} md={6}>
+          <Container fluid style={{ marginTop: '30px', marginBottom: '150px' }}>
+            <Row
+              xs={1}
+              sm={3}
+              md={5}
+              lg={7}
+              xl={9}
+              xxl={11}
+              className="g-3"
+              style={{ gap: '1rem' }}
+            >
+              {menus.map((menu) => (
+                <Col key={menu.id} margin="5px">
+                  <Menu menuObj={menu} />
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default CafeDetail;
--- src/pages/CafeList.jsx
+++ src/pages/CafeList.jsx
@@ -1,36 +1,35 @@
- /* eslint-disable */
-import {Link} from "react-router-dom";
+/* eslint-disable */
+import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
-import { Button, Col, Container, Form, Row, Card } from "react-bootstrap";
+import { Button, Col, Container, Form, Row, Card } from 'react-bootstrap';
 
 //백엔드에 필요한 것들
 import axios from 'axios';
 import { API_URL } from '../constants';
 
-
 function CardArray(parmData) {
-  const data = parmData.parmData //꺼내기
+  const data = parmData.parmData; //꺼내기
 
-  if(data.length== 0) {
-    console.log("빈 배열!")
-    return
-  } else{
-    const valueList = Object.values(data)
-    console.log("배열 !!!",valueList)
+  if (data.length == 0) {
+    console.log('빈 배열!');
+    return;
+  } else {
+    const valueList = Object.values(data);
+    console.log('배열 !!!', valueList);
 
     // localhost:3000/cafelist/detail/1 호출
     const tags = valueList.map((item, idx) => (
-        <Link to={`cafe/detail/${item.id}/`}> 
+      <Link to={`/cafelist/detail/${item.id}/`}>
         <Card key={idx}>
           <Row>
             <Col>
-            <Card.Body>
-              <Card.Title sm={4}>{item.name}</Card.Title>
-              <Card.Text sm={4}>
-                <h5>{item.location}</h5>
-                {item.info}
-              </Card.Text>
-            </Card.Body>
+              <Card.Body>
+                <Card.Title sm={4}>{item.name}</Card.Title>
+                <Card.Text sm={4}>
+                  <span>{item.location}</span>
+                  {item.info}
+                </Card.Text>
+              </Card.Body>
             </Col>
             <Col>
               {/* <img src={require("./assets/point_1000.png")} width="90%" alt="img"/>  //이미지가 들어갈 자리 */}
@@ -38,79 +37,82 @@ function CardArray(parmData) {
           </Row>
         </Card>
       </Link>
-    ))
-    
-    return(
-      <Container>
-        {tags}
-      </Container>
-    );
+    ));
+
+    return <Container>{tags}</Container>;
   }
 }
 
 function CafeList() {
-  const [searchValue, setSearchValue] = useState(""); //검색값
+  const [searchValue, setSearchValue] = useState(''); //검색값
   const [searchedList, setSearchedList] = useState([]);
 
   //검색창의 값이 바뀔 때마다 실행됨. (검색값 저장)
   const handleChange = (e) => {
     e.preventDefault();
-    console.log('handleChange() 불려짐')
+    console.log('handleChange() 불려짐');
     setSearchValue(e.target.value);
-  }
+  };
 
   //[검색] 버튼 눌렀을 때 handleClick() 실행됨. (데이터 전송)
   const handleClick = async (e) => {
     e.preventDefault();
-    console.log('handleClick() 불려짐')
-    console.log(searchValue)
+    console.log('handleClick() 불려짐');
+    console.log(searchValue);
     await axios //axios로 서버에 요청 보내는 부분 시작!
-      .post(`${API_URL}cafe_list/`, {  //백엔드의 url 요청과 보낼 data 객체 
+      .post(`${API_URL}cafe_list/`, {
+        //백엔드의 url 요청과 보낼 data 객체
         search_value: searchValue,
-      }).then((response) => { 
-        console.log(response.data.search_result)
-        
-        if(response.data.search_result === "null") {
-          console.log("검색 결과가 없습니다.")
-        } else{
-          let jsonResult = JSON.parse(response.data.search_result);
-          setSearchedList(jsonResult) //객체가 저장됨
-        }
-      }).catch((err) => { 
-        //에러 잡는 구문
-        console.log("90번 줄 에러입니다 : ", err);
-      });
-  }
+      })
+      .then((response) => {
+        console.log(response.data.search_result);
 
+        if (response.data.search_result === 'null') {
+          console.log('검색 결과가 없습니다.');
+        } else {
+          let jsonResult = JSON.parse(response.data.search_result);
+          setSearchedList(jsonResult); //객체가 저장됨
+        }
+      })
+      .catch((err) => {
+        //에러 잡는 구문
+        console.log('90번 줄 에러입니다 : ', err);
+      });
+  };
 
   return (
     <>
-    {/* 검색창 */}
-    <Container className="mt-5">
-      <Row>
-        <Col>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2 rounded-pill"
-              aria-label="Search"
-              id="inputValue"
-              onChange={handleChange}
-              value={searchValue}
-            />
-            <Button type="submit" className="rounded-pill" variant="outline-primary" onClick={handleClick} >
-              Search
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+      {/* 검색창 */}
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2 rounded-pill"
+                aria-label="Search"
+                id="inputValue"
+                onChange={handleChange}
+                value={searchValue}
+              />
+              <Button
+                type="submit"
+                className="rounded-pill"
+                variant="outline-primary"
+                onClick={handleClick}
+              >
+                Search
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
 
-    {/* 카드 group */}
-    {/* <Container> */}
+      {/* 카드 group */}
+      {/* <Container> */}
       <CardArray parmData={searchedList}></CardArray>
-    {/* </Container>     */}
+      {/* </Container>     */}
     </>
   );
 }
