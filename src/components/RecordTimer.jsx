c5ffb025d9958cde35f0f4f851c6d03dde001e17
jm3789 <jm37890@gmail.com>
2023-06-18 04:29:15 +0900

    #36 타이머 시작/일시정지 버튼 추가

--- src/components/RecordTimer.jsx
+++ src/components/RecordTimer.jsx
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
+import { GrPlay, GrPause } from "react-icons/gr";
 
 function RecordTimer({ detail }) {
   const [timer, settimer] = useState();
 
+  const [isTimerRunning, setTimerRunning] = useState(false);
+
+  const handleTimerRunning = () => {
+    if (isTimerRunning) {
+      setTimerRunning(false);
+    } else {
+      setTimerRunning(true);
+    }
+  };
+
   return (
-    <div style={{ fontSize: '40px', fontWeight: 'bold', color: 'mediumpurple' }}>
-      {/* {detail.duration} */}
-      듀레이션듀레이션
-    </div>
-    // button
+    <>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ fontSize: '40px', fontWeight: 'bold', color: 'mediumpurple' }}>
+          {/* {detail.duration} */}
+          듀레이션듀레이션
+        </div>
+        {isTimerRunning ? (
+          <GrPause style={{ marginLeft:'15px' }} size="30" onClick={handleTimerRunning} />
+        ) : (
+          <GrPlay style={{ marginLeft:'15px' }} size="30" onClick={handleTimerRunning} />
+        )}
+      </div>
+    </>
   );
 }
 
